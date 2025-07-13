01afeed483534aaba190de97b2d414ad07bd6f98
5522551834 <s225221834@deakin.edu.au>
2025-07-13 15:58:13 +1000

    edit scripts js

--- Week-1/JS/scripts.js
+++ Week-1/JS/scripts.js
@@ -7,5 +7,6 @@ document.getElementById("heading").innerHTML = textsArray[number];
 }
 
 function getRandomNumberBetween(min,max){
-return Math.floor(Math.random()*(max-min+1)+min);
+return Math.floor(Math.random() * (max-min+1) + min);
 }
+
