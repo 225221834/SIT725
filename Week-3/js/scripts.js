a7c7006e5da0e527250898f386bef6a98e8ebc38
5522551834 <s225221834@deakin.edu.au>
2025-07-26 21:06:03 +1000

    Changes made to directories

--- Week-3/js/scripts.js
+++ Week-3/js/scripts.js
@@ -2,9 +2,10 @@
 const clickMe = () => {
 alert("Thanks for clicking me. Hope you have a nice day!")
 }
+
 $(document).ready(function(){
-$('.materialboxed').materialbox();
-$('#clickMeButton').click(()=>{
-clickMe();
+    $('.materialboxed').materialbox();
+    $('#clickMeButton').click(()=>{
+        clickMe();
 })
 });
