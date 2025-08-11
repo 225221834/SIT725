0436d2a5dade96a709f196160d241b74b795e786
5522551834 <s225221834@deakin.edu.au>
2025-08-11 18:50:01 +1000

    update corrected

--- Week-5/routes/projects.js
+++ Week-5/routes/projects.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+// Simple GET route
 router.get('/', (req, res) => {
 res.send('Welcome to the Projects Room');
 });
+
+module.exports = router;
 
--- Week-5/server.js
+++ Week-5/server.js
@@ -1,69 +1,19 @@
 
-/*var express = require("express")
-var app = express()
-var port = process.env.port || 3004
-const mongoose = require('mongoose');
-
-//middleware 
-app.use(express.static(__dirname+'/public'))
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-mongoose.connect('mongodb://localhost:27017/myprojectDB', {
-useNewUrlParser: true,
-useUnifiedTopology: true,
-});
-    mongoose.connection.on('connected', () => {
-    console.log('Connected to MongoDB');
-});
-// 2. Define your schema and model
-    const ProjectSchema = new mongoose.Schema({
-    title: String,
-    image: String,
-    link: String,
-    description: String,
-});
-const Project = mongoose.model('Project', ProjectSchema);
-    // 3. REST API route
-    app.get('/api/projects', async (req, res) => {
-    const projects = await Project.find({});
-    res.json({ statusCode: 200, data: projects, message: 'Success' });
-});
-    
-
-/*const cardList = [
-{
-    title: "Pattern 2",
-    image: "images/image2.jpg",
-    link: "About Pattern 2",
-    desciption: "Demo desciption about Pattern 2"
-},
-{
-    title: "Pattern 3",
-    image: "images/image3.jpg",
-    link: "About Pattern 3",
-    desciption: "Demo desciption about Pattern 3"
-}
-]*/
- /*   app.get('/api/projects',(req,res) => {
-    res.json({statusCode: 200, data: cardList, message:"Success"})
-})*/
 var express = require("express")
 var app = express()
-var port = process.env.port || 3000;
+var PORT = 3000;
 
-app.listen(port,()=>{
-console.log("App listening to: "+port)
+// Mount the route file
+let projectsRoute = require('./routes/projects')
+
+//Mount the route at /api/projects
+app.use('/api/projects', projectsRoute);
+
+// Root the route
+app.get('/', (req, res) => {
+res.send('Welcome to the Home Page!');
 });
 
-/*Week5--- */
-
-// Import route file
-//const projectsRoute = require('./routes/projects');
-
-// Mount the route at /api/projects
-let projectsRoute = require('./routes/projects')
-app.use('/api/projects', projectsRoute);
-app.use('/api/projects',projectsRoute)
-
-
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
